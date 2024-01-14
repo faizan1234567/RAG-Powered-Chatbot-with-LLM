159a6ccae6ae50aeff4cfea444fcc182f02e4181
MFaizan <mohammadfaizantanoli@gmail.com>
2024-01-14 15:22:29 +0500

    added documents spliting and dividing into chunks

--- main.py
+++ main.py
@@ -21,7 +21,48 @@ def load_data(file_name: str = "text_data.csv"):
     return df
 
 
+def chunk_dataset(dataset: pd.DataFrame, chunk_size: int = 1000, chunk_overlap: int = 0):
+    """
+    divide the dataset into chunk of documents due to limited context window size of
+    an LLM by using langchain functions. 
+
+    Parameters
+    ----------
+    dataset: a dataframe containing information
+    chunk_size: number of elements in each chunk
+    chunk_overlap: chunk overlap size
+
+    Return
+    ------
+    list: list of chunks
+    """
+    chunks = DataFrameLoader(
+           dataset, page_content_column= "body"
+    ).load_and_split(text_splitter = 
+                     RecursiveCharacterTextSplitter(chunk_size = chunk_size, 
+                                                    chunk_overlap = chunk_overlap,
+                                                    length_function = len), 
+                     )
+    
+    # for each chunk retreive important info
+    for doc in chunks:
+        title = doc.metadata["title"]
+        description = doc.metadata["description"]
+        content = doc.page_content
+        url = doc.metadata["url"]
+        updated_content = f"TITLE: {title}\DESCRIPTION: {description}\BODY: {content}\nURL: {url}"
+        doc.page_content = updated_content
+    return chunks
+    
+
+
+
+
+
+
 
 if __name__ == "__main__":
     df = load_data("text_data.csv")
-    print(df.columns)
+    text_chunks = chunk_dataset(df)
+    # test a chunk
+    print(text_chunks[74])
