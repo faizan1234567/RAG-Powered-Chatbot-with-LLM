9a0c0d654eafc3751ebc7adee51c794b0ce5a2de
MFaizan <mohammadfaizantanoli@gmail.com>
2024-02-18 19:18:53 +0500

    updated the utils

--- utils.py
+++ utils.py
@@ -16,8 +16,14 @@ import databases.DLAIUtils as du
 import time
 import warnings
 from tqdm.auto import tqdm
+
+from langchain_community.document_loaders import PyPDFLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
 # ignore warnings
 warnings.filterwarnings('ignore')
+
+# enable cuda if available
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
 # load the txt file 
 def load_text_file(file: str):
@@ -32,10 +38,45 @@ def load_text_file(file: str):
         context = f.read()
     return context 
 
-# enable cuda if available
-device = 'cuda' if torch.cuda.is_available() else 'cpu'
+def split_text(pdf_file: str, chunk_size: int = 500, 
+               chunk_overlap: int = 50):
+    """
+    split the text into small chunks for creating embeddings
+    --------------------------------------------------------
+
+
+    Parameters
+    ----------
+    pdf_file: a pdf file that provides the context to ingest in the prompt
+    chunk_size: text chunk_size to be used for splitting the text
+    chunk_overlap: how of the text between two consectuive docs should overalp
+    ideally it's less than 100
+
+    Return
+    ------
+    context: list (list of chunked text ready to be used for embbedings)
+    """
+    # load the pdf file and split into a docs
+    loader = PyPDFLoader(pdf_file)
+    docs = loader.load_and_split()
+
+    # Chunk text
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=chunk_size,
+                                                   chunk_overlap=chunk_overlap)
+    chunked_documents = text_splitter.split_documents(docs)
+
+    # store the context in the list
+    context = []
+    for doc in chunked_documents:
+        page_content = doc.page_content.replace("\n", " ")
+        context.append(page_content)
+    return context
+
+
+
+
 #TODO: to create a vec database
-def create_vecdb(batch_size: int = 200, vec_limit: int = 100000,
+def create_vecdb(batch_size: int = 4, vec_limit: int = 100000,
                  device: str = 'cuda'):
     pass
 
